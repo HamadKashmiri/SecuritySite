9d143c55268e36afbb24bc6478278b9d8e523fb1
Hamad <k.hamad@hotmail.co.uk>
2020-10-20 22:33:39 +0100

    setup complete template added

--- Forum/views.py
+++ Forum/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+questions = [
+    {
+        'author': 'Hamad Kashmiri',
+        'title': 'Question1',
+        'content':  'question content',
+        'date':     'august 28, 2018'
+    },
+    {
+        'author': 'Hamad Kashmiri',
+        'title': 'Question2',
+        'content':  'question content',
+        'date':     'august 28, 2018'
+    }
+]
+
 def home(request):
-    return HttpResponse("<h1>Forum Home</h1>")
+    context = {
+        'questions': questions
+    }
+
+    return render(request, 'forum/home.html', context)
